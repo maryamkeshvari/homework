8fd08e8f34d826385ce8ca0f9c656806f0faea43
maryamkeshvari <maryamkeshvari@gmail.com>
2019-02-18 01:40:46 +0400

    master

--- p1.py
+++ p1.py
@@ -12,9 +12,16 @@ class Chatbot:
             random_index = random.randint(0, len(self.standard_answers) - 1)
             print(self.standard_answers[random_index])
 
+    def learn(self,text):
+        if "?" in text:
+            random_index = random.randint(0, len(self.question_answers) - 1)
+            question_answers.append(random_index)
+        else:
+            random_index = random.randint(0, len(self.standard_answers) - 1)
+            standard_answers.append(random_index)
 
-standard_answers=["yes","no"]
-question_answers=["good","bad"]
+standard_answers=["yes","no","it is interesting"]
+question_answers=["good","bad","maybe","who knows","ok","only god know"]
 
 chat=Chatbot(standard_answers,question_answers)
 while True:
